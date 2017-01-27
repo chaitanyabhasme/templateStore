9e335f56f83325b89b32e0decca1594cb34b27b3
Chaitanya Bhasme <chaitanya6790@gmail.com>
2017-01-26 21:18:36 -0500

    convert data to json files

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -12,10 +12,18 @@ angular.module('templateStore.templates', ['ngRoute'])
     });
 }])
 
-.controller('TemplatesCtrl', ['$scope', function ($scope) {
-    $scope.imgColors = ['blue', 'gray', 'green', 'orange', 'purple', 'red'];
+.controller('TemplatesCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('json/templates.json').then(function (response) {
+      $scope.templates = response.data;
+    });
 
 }])
-  .controller('TemplateDetailCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
-    $scope.color = $routeParams.templateId;
+  .controller('TemplateDetailCtrl', ['$scope', '$routeParams', '$http', '$filter', function ($scope, $routeParams, $http, $filter) {
+    var templateId = $routeParams.templateId;
+    $http.get('json/templates.json').then(function (response) {
+      $scope.template = $filter('filter')(response.data, function (d) {
+        return d.id == templateId;
+      })[0];
+      $scope.mainImage = $scope.template.images[0].name;
+    });
 }]);
